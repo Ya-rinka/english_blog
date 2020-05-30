0a976aac3183c1783981016f62c81622b4f0ba1d
Yaryna <yarinaned@gmail.com>
2020-05-30 23:20:51 +0300

    New and show methods added

---
+++ app/controllers/stat_pages_controller.rb
@@ -0,0 +1,6 @@
+class StatPagesController < ApplicationController
+
+    def about
+    end
+    
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+ root 'teachers#index'
+
+ get 'about' => 'stat_pages#about'
+ resources :teachers
 end
